7e5ac5db4d92e38a2dedfa385df5ecbdbb8f3e87
Michael Langford <mikelangford@licsystems.com>
2022-06-10 19:57:18 -0700

    need to fix this thing

--- simpleCounter.py
+++ simpleCounter.py
@@ -1,11 +1,31 @@
 import tkinter as tk
 from tkinter import ttk
 
-
+# main window
 root = tk.Tk()
 root.title("simple counter app")
 root.geometry('360x360+800+300')
 
+# incrementer function
+#num = 0
+def incrementer(num = 1):
+    num = num + 1
+
+    
+########### fix this shit #####################################
+########### incrementer is not working ########################
 
 
+ # label number showing count
+ 
+label = tk.Label(root, text= ' count is: ${num}')
+label.pack()
+
+
+
+# add increment button
+button = tk.Button(root, text="increment", command=incrementer)
+button.pack()
+
+# run main program
 root.mainloop()
